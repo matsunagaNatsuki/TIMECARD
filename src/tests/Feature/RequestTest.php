df5db15dc9f181a60f9cd6d91897ef0bd1d0f9ce
松永菜月 <dicaiyue856@gmail.com>
2025-10-18 18:02:26 +0900

    変更

--- src/tests/Feature/RequestTest.php
+++ src/tests/Feature/RequestTest.php
@@ -8,6 +8,7 @@ use App\Models\AttendanceRequest;
 use Database\Seeders\DatabaseSeeder;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Tests\TestCase;
+use Carbon\Carbon;
 
 class RequestTest extends TestCase
 {
@@ -99,6 +100,41 @@ class RequestTest extends TestCase
             $response->assertSeeText($request->remarks);
             $response->assertSeeText($user->name);
         }
+    }
+
+    public function test_revise_application_application_page_detail_check()
+    {
+        $admin = User::factory()->create([
+            'role' => 'admin',
+            'email_verified_at' => now(),
+        ]);
+
+        $user = User::factory()->create(['role' => 'users']);
+
+        $attendance = Attendance::factory()->create(['user_id' => $user->id]);
+
+        $request = AttendanceRequest::factory()->create([
+            'attendance_id' => $attendance->id,
+            'request_by' => $user->id,
+            'clock_in' => noW()->setTime(10,0),
+            'clock_out' => now()->setTime(19,0),
+            'remarks' => 'シフト変更',
+            'status' => 'pending',
+        ]);
+
+        $response = $this->actingAs($admin)->get(
+            route('admin.application', $request->id)
+        );
+        $response->assertStatus(200);
+
+        $response->assertSeeText($user->name);
+        $response->assertSeeText(Carbon::parse($attendance->date)->format('Y年'));
+        $response->assertSeeText(Carbon::parse($attendance->date)->format('n月j日'));
+        $response->assertSee($request->clock_in->format('H:i'));
+        $response->assertSee($request->clock_out->format('H:i'));
+        $response->assertSeeText('シフト変更');
+
+
     }
 
 
